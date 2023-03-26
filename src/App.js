68de1abd960a9efc3217903c1e3175a231b6d7c1
reactnetau <polyrhythmm@gmail.com>
2023-03-26 23:56:23 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,21 +3,16 @@ import * as React from 'react';
 import {
   createBrowserRouter,
   RouterProvider,
-  Route,
-  Link,
 } from "react-router-dom";
 import HomePage from './pages/HomePage/HomePage';
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: (
       <HomePage />
     ),
-  },
-  {
-    path: "about",
-    element: <div>About</div>,
-  },
+  }
 ]);
 
 export default function App() {
